0772c7f3149cc87080ac442bf5c6bb0593e56e9d
felixlohmann <feloh@gmx.de>
2022-07-21 11:16:37 +0200

    Vue-Meta Package verbessert Compression hinzugefügt

--- monshiners_alpha/vue.config.js
+++ monshiners_alpha/vue.config.js
@@ -1,3 +1,4 @@
+const zlib = require("zlib");
 module.exports = {
   chainWebpack: config => {
     config.plugin('VuetifyLoaderPlugin').tap(args => [{
@@ -18,6 +19,25 @@ module.exports = {
       fallbackLocale: '(de)',
       localeDir: 'locales',
       enableInSFC: true
+    },
+    compression:{
+      brotli: {
+        filename: '[file].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      }
     }
   }
 }
